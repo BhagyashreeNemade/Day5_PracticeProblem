04c7dfd84b708056b221818eeceacce2fe5f509c
bhagyashree2000 <bhagyashreenemade2000@gmail.com>
2022-08-03 10:42:43 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,11 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Day 5 Practice Problems ");
-            Console.WriteLine("Enter the number ");
-            int N = Convert.ToInt32(Console.ReadLine());
-
-            string Output = EvenOdd.CheckEvenOdd(N);
-            Console.WriteLine(Output);
+            VowelOrConsonant.CheckVowelAndConsonant();
         }
     }
 }
