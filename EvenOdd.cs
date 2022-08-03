625a15c649700132d726fb5bd5b1265114dfa3bd
bhagyashree2000 <bhagyashreenemade2000@gmail.com>
2022-08-03 10:34:57 +0530

    Added class for Ecen and Odd

---
+++ EvenOdd.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5_PracticeProblem
+{
+    internal class EvenOdd
+    {
+        /// <summary>
+        /// Checks for Even and Odd number.
+        /// </summary>
+        /// <param name="N">The n.</param>
+        /// <returns></returns>
+        public static string CheckEvenOdd(int N)
+        {
+            if (N % 2 == 0)
+            {
+
+                return $"{N} is an even number";
+
+            }
+            else
+            {
+
+                return $"{N} is an Odd number";
+            }
+        }
+    }
+}
