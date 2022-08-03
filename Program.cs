b2993e75ac7f91e36f61bc1b6a62d1ccc9078017
bhagyashree2000 <bhagyashreenemade2000@gmail.com>
2022-08-03 10:27:49 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,11 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Day 5 Practice Problems ");
-            Console.WriteLine("Enter the Dividend ");
-            int dividend = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the Devisor ");
-            int devisor = Convert.ToInt32(Console.ReadLine());
-            QuotientAndReminder.FindQuotientAndReminder(dividend, devisor);
+            Console.WriteLine("Enter the a ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the b ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            SwapNumber.SwapNum(a, b);
         }
     }
 }
